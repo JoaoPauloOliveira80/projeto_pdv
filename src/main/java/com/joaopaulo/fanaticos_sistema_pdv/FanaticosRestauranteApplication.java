49f1e9feff56ace756ab22ec0e841b99eb6b3203
Dundaki <vigjoaopaulo@gmail.com>
2023-05-17 08:01:21 -0300

    conexao com o banco

---
+++ src/main/java/com/joaopaulo/fanaticos_sistema_pdv/FanaticosRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.joaopaulo.fanaticos_sistema_pdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanaticosRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanaticosRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaopaulo/fanaticos_sistema_pdv/FanaticosRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopaulo.fanaticos_sistema_pdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanaticosRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
